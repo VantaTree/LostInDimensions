7982412587908f8d0ce6f7b0dff7b58653dfca59
VantaTree <vantatree@gmail.com>
2023-01-06 13:22:03 +0300

    added player animations

--- modules/player.py
+++ modules/player.py
@@ -12,9 +12,14 @@ class Player:
         self.master.player = self
         self.screen = pygame.display.get_surface()
 
-        self.original_image = pygame.image.load("graphics/player/test.png").convert_alpha()
-        self.image = self.original_image
+        self.animations = import_sprite_sheets("graphics/player/anims")
+        self.animation = self.animations["idle"]
+        # self.original_image = pygame.image.load("graphics/player/test.png").convert_alpha()
+        self.image = self.animation[0]
         self.rect = self.image.get_rect()
+
+        self.anim_index = 0
+        self.anim_speed = 0.15
 
         self.slant_rect = pygame.Rect(0, 0, 16, 3)
         self.hitbox = FRect(0, 0, 16, 32)
@@ -30,12 +35,36 @@ class Player:
         self.moving = False
         self.can_jump = True
         self.on_ground = True
+        self.jumping = False
+        self.landing = False
 
         self.JUMP_TIMER = CustomTimer()
 
     def update_image(self):
 
-        self.image = pygame.transform.flip(self.original_image, self.facing_right, False)
+        if self.jumping: state = "jump"
+        elif not self.on_ground: state = "midair"
+        elif self.moving: state = "run"
+        else:
+            state = "idle"
+            # if random.randint(1, 10) > 3:
+            #     state += "_blink"
+
+        try:
+            image = self.animations[state][int(self.anim_index)]
+        except IndexError:
+            image = self.animations[state][0]
+            self.anim_index = 0
+
+            if self.jumping: self.jumping = False
+            if self.landing: self.landing = False
+
+        if self.moving: self.anim_speed = 0.15
+        else: self.anim_speed = 0.08
+
+        self.anim_index += self.anim_speed *self.master.dt
+
+        self.image = pygame.transform.flip(image, not self.facing_right, False)
         self.rect.midbottom = self.hitbox.midbottom
 
     def get_input(self):
@@ -55,6 +84,8 @@ class Player:
             self.velocity.y = -self.jump_power
             self.can_jump = False
             self.JUMP_TIMER.start(100)
+            self.jumping = True
+            self.anim_index = 0
 
         self.moving = bool(self.input_x)
 
@@ -74,9 +105,14 @@ class Player:
         self.hitbox.centerx += self.velocity.x * self.master.dt
         do_collision(self, self.master.game.level, 0, self.master)
         self.hitbox.centery += self.velocity.y * self.master.dt
+        self.power_land = 0
+        if not self.on_ground: self.power_land = self.velocity.y
         self.on_ground = False
         do_collision(self, self.master.game.level, 1, self.master)
         do_collision(self, self.master.game.level, 2, self.master)
+        if self.power_land and self.on_ground and self.velocity.y > 1:
+            self.landing = True
+            self.anim_index = 0
 
     def process_events(self):
 
@@ -97,6 +133,9 @@ class Player:
         self.apply_force()
         self.move()
         self.update_image()
+
+        self.master.debug("pos: ", self.hitbox.midbottom)
+
 
 def do_collision(player:Player, level, axis, master):
 
@@ -142,29 +181,19 @@ def do_collision(player:Player, level, axis, master):
             elif axis == 2 and player.velocity.y >= 0: # slopes
 
                 if not rect.colliderect(player.slant_rect): continue
-                # if not rect.collidepoint(player.hitbox.bottomleft) and not\
-                #         rect.collidepoint(player.hitbox.bottomright):continue
 
-
+                relx = None
                 if get_xy(level.up_slant_coll, x, y):
                     relx = player.hitbox.right - rect.left
+                elif get_xy(level.down_slant_coll, x, y):
+                    relx = rect.right - player.hitbox.left
+                    
+                if relx is not None:
                     if relx > TILESIZE:
                         player.hitbox.bottom = rect.top
                         player.on_ground = True
                         player.velocity.y = 0
                     elif player.hitbox.bottom > y*TILESIZE-relx+TILESIZE:
-                        player.hitbox.bottom = y*TILESIZE-relx+TILESIZE
-                        player.on_ground = True
-                        player.velocity.y = 0
-
-                if get_xy(level.down_slant_coll, x, y):
-                    relx = rect.right - player.hitbox.left
-                    master.debug("On slope: ", True)
-                    if relx > TILESIZE:
-                        player.hitbox.bottom = rect.top
-                        player.on_ground = True
-                        player.velocity.y = 0
-                    if player.hitbox.bottom > y*TILESIZE-relx+TILESIZE:
                         player.hitbox.bottom = y*TILESIZE-relx+TILESIZE
                         player.on_ground = True
                         player.velocity.y = 0
